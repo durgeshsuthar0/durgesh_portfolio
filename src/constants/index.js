7aa533468891b11e21fdddce8fa39157f9866d76
Durgesh <durgesh.suthar@qpaix.com>
2025-02-25 11:00:05 +0530

    updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -115,6 +115,19 @@ const technologies = [
 ];
 
 const experiences = [
+   {
+    title: "ReactJS Developer",
+    company_name: "Qpaix Infitech PVT LTD, Ahmedabad",
+    icon: starbucks,
+    iconBg: "#ffffff",
+    date: "Jun 2024 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
   {
     title: "React.js Developer intern.",
     company_name: "Spectrics Solutins PVT LTD, Ahemdabad",
@@ -128,19 +141,7 @@ const experiences = [
       "Participating in code reviews and providing constructive feedback to developers.",
     ],
   },
-  // {
-  //   title: "React Native Developer",
-  //   company_name: "Tesla",
-  //   icon: starbucks,
-  //   iconBg: "#383E56",
-  //   date: "Jan 2021 - Feb 2022",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
+ 
   // {
   //   title: "Web Developer",
   //   company_name: "Shopify",
